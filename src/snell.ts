f659fdfe12a27209dc977ca4708e7e76621df45f
MamoruDS <mamoruds.io@gmail.com>
2020-08-28 23:00:37 +0900

    added: snell.ts for snell support in clash

---
+++ src/snell.ts
@@ -0,0 +1,44 @@
+import { Optional, assignDefault } from './utils'
+import { BaseProxy } from './proxy'
+
+type SnellOptions = {
+    mode: 'http' | 'tls'
+    host?: string
+}
+
+const defaultSnellOptions: Required<SnellOptions> = {
+    mode: 'http',
+    host: undefined,
+}
+
+export type SnellProxyCtor = {
+    new (
+        tag: string,
+        server: string,
+        port: number,
+        psk: string,
+        obfsOpts?: Optional<SnellOptions>
+    ): SnellProxy
+}
+
+export class SnellProxy extends BaseProxy {
+    public readonly psk?: string
+    public readonly obfsMode?: 'http' | 'tls'
+    public readonly obfsHost?: string
+
+    constructor(...P: ConstructorParameters<SnellProxyCtor>)
+    constructor(
+        tag: string,
+        server: string,
+        port: number,
+        psk: string,
+        options: Optional<SnellOptions> = {},
+        policyGroup: string[] = []
+    ) {
+        super(tag, server, port, policyGroup)
+        const _options = assignDefault(defaultSnellOptions, options)
+        this.psk = psk
+        this.obfsMode = _options.mode
+        this.obfsHost = _options.host
+    }
+}
