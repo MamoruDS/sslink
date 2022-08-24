53ba8ae3cd571b99a90e65a05e782a6fe2096022
MamoruDS <mamoruds.io@gmail.com>
2022-08-25 08:47:35 +0900

    added: `BaseProxy` for proxies build

---
+++ src/proxies/base.ts
@@ -0,0 +1,27 @@
+import { NotImplementedError } from '../constants'
+import { Supported } from '../types'
+
+type BaseProperties = {
+    tag: string
+    server: string
+    port: number
+    groups: string[]
+}
+
+class BaseProxy<P extends object = {}> {
+    public readonly properties: P & BaseProperties
+
+    constructor(properties: P & BaseProperties) {
+        this.properties = properties
+    }
+
+    get prop(): P & BaseProperties {
+        return this.properties
+    }
+
+    public parse(platform: Supported): string | undefined {
+        throw new NotImplementedError()
+    }
+}
+
+export { BaseProxy }
