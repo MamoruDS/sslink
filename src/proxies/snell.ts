5f1dc7be0f3ece83175d9b20b222e2e3d04a198f
MamoruDS <mamoruds.io@gmail.com>
2022-08-25 08:52:03 +0900

    added: `SnellProxy` for snell proxy support

---
+++ src/proxies/snell.ts
@@ -0,0 +1,60 @@
+import * as yaml from 'js-yaml'
+import { NotSupportedError } from '../constants'
+import { Supported } from '../types'
+import { optionalArgs as oa, undefinedFreeJoin } from '../utils'
+import { BaseProxy } from './base'
+
+type SnellServerVersion = 1 | 2 | 3
+
+type SnellProperties = {
+    psk: string
+    version?: SnellServerVersion
+    mode: 'http' | 'tls'
+    host?: string
+    fast_open: boolean
+}
+
+class SnellProxy extends BaseProxy<SnellProperties> {
+    parse(platform: Supported): string {
+        if (platform === Supported.Clash) {
+            const p: Record<
+                string,
+                | string
+                | number
+                | {
+                      [key in string]: string
+                  }
+            > = {}
+            p['name'] = this.prop.tag
+            p['type'] = 'snell'
+            p['server'] = this.prop.server
+            p['port'] = this.prop.port
+            p['psk'] = this.prop.psk
+            if (typeof this.prop.version != undefined) {
+                p['version'] = this.prop.version
+            }
+            if (this.prop.mode) {
+                p['obfs-opts'] = {
+                    mode: this.prop.mode,
+                    host: this.prop.host,
+                }
+            }
+            return yaml.dump([p])
+        } else if (platform === Supported.Surge) {
+            const p: (string | number)[] = []
+            p.push('snell')
+            p.push(this.prop.server)
+            p.push(this.prop.port)
+            p.push(oa('psk', this.prop.psk))
+            p.push(oa('obfs', this.prop.mode))
+            p.push(oa('obfs-host', this.prop.host))
+            p.push(oa('version', this.prop.version))
+            p.push(oa('tfo', this.prop.fast_open))
+            return this.prop.tag + ' = ' + undefinedFreeJoin(p, ', ')
+        } else {
+            throw new NotSupportedError(platform)
+        }
+    }
+}
+
+export { SnellProxy }
