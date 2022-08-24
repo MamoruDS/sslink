175df6b0d149cd7a8cc317c70bddfbb3a23baf68
MamoruDS <mamoruds.io@gmail.com>
2022-08-25 08:59:43 +0900

    added: `LoadBalancePolicy` for load-balance policy support

---
+++ src/policies/p_loadbalance.ts
@@ -0,0 +1,54 @@
+import * as yaml from 'js-yaml'
+import { NotSupportedError } from '../constants'
+import { Supported } from '../types'
+import { optionalArgs, undefinedFreeJoin } from '../utils'
+import { BasePolicy, ValidPolicyItem } from './base'
+
+class LoadBalancePolicyProperties {
+    url?: string
+    interval?: number
+    persistent?: boolean // surge only
+}
+
+class LoadBalancePolicy extends BasePolicy {
+    public properties: LoadBalancePolicyProperties
+
+    constructor(
+        name: string,
+        properties: LoadBalancePolicyProperties,
+        items: ValidPolicyItem[] = []
+    ) {
+        super(name, items)
+        this.properties = properties
+    }
+
+    get prop(): LoadBalancePolicyProperties {
+        return this.properties
+    }
+
+    parse(platform: Supported): string {
+        const proxies: string[] = this._parseItems(platform)
+        if (platform === Supported.Clash) {
+            const p = {
+                name: this.name,
+                type: 'load-balance',
+                url: this.prop.url,
+                interval: this.prop.interval,
+                proxies,
+            }
+            return yaml.dump([p])
+        } else if (platform === Supported.Surge) {
+            const p: string[] = []
+            p.push('load-balance')
+            p.push(...proxies)
+            p.push(
+                optionalArgs('persistent', this.prop.interval ? 1 : undefined)
+            )
+            return this.name + ' = ' + undefinedFreeJoin(p, ', ')
+        } else {
+            throw new NotSupportedError(platform)
+        }
+    }
+}
+
+export { LoadBalancePolicy }
