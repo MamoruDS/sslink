874e334b51556260dad7d91e81b5ebb683f1ec69
MamoruDS <mamoruds.io@gmail.com>
2022-08-25 08:43:04 +0900

    added: src/constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,52 @@
+import { SpecialProxy, RuleType } from './types'
+
+const REJECT = new SpecialProxy('REJECT')
+const DIRECT = new SpecialProxy('DIRECT')
+
+const RULE_SUPPORT_MAP_CLASH = {
+    [RuleType.DOMAIN]: 'DOMAIN',
+    [RuleType.DOMAIN_SUFFIX]: 'DOMAIN-SUFFIX',
+    [RuleType.DOMAIN_KEYWORD]: 'DOMAIN-KEYWORD',
+    [RuleType.IP_CIDR]: 'IP-CIDR',
+    [RuleType.IP_CIDR_6]: 'IP-CIDR6',
+    [RuleType.GEOIP]: 'GEOIP',
+    [RuleType.SRC_IP_CIDR]: 'SRC-IP-CIDR', // TODO:
+    [RuleType.DEST_PORT]: 'DST-PORT',
+    [RuleType.SRC_PORT]: 'SRC-PORT',
+    [RuleType.PROCESS_NAME]: 'PROCESS-PATH',
+    [RuleType.FINAL]: 'MATCH',
+}
+
+const RULE_SUPPORT_MAP_SURGE = {
+    [RuleType.DOMAIN]: 'DOMAIN',
+    [RuleType.DOMAIN_SUFFIX]: 'DOMAIN-SUFFIX',
+    [RuleType.DOMAIN_KEYWORD]: 'DOMAIN-KEYWORD',
+    [RuleType.IP_CIDR]: 'IP-CIDR',
+    [RuleType.IP_CIDR_6]: 'IP-CIDR6',
+    [RuleType.GEOIP]: 'GEOIP',
+    [RuleType.SRC_IP_CIDR]: 'SRC-IP',
+    [RuleType.DEST_PORT]: 'DEST-PORT',
+    [RuleType.SRC_PORT]: 'SRC-PORT',
+    [RuleType.PROCESS_NAME]: 'PROCESS-NAME',
+    [RuleType.FINAL]: 'FINAL',
+}
+
+class NotImplementedError extends Error {}
+class NotSupportedError extends Error {
+    platform: any
+    constructor(platform: any) {
+        super()
+        this.platform = platform
+    }
+}
+
+export {
+    SpecialProxy,
+    RULE_SUPPORT_MAP_CLASH,
+    RULE_SUPPORT_MAP_SURGE,
+    NotImplementedError,
+    NotSupportedError,
+    //
+    DIRECT,
+    REJECT,
+}
