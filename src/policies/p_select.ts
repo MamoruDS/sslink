cb89dcf9af387c935da562a33bd2e64a4216f1bb
MamoruDS <mamoruds.io@gmail.com>
2023-11-04 17:03:02 +0900

    add: policy.select parse for loon, stash and surfboard

--- src/policies/p_select.ts
+++ src/policies/p_select.ts
@@ -20,11 +20,26 @@ class SelectPolicy extends BasePolicy {
                 proxies,
             }
             return yaml.dump([p])
+        } else if (platform === Supported.Loon) {
+            // ref: https://loon0x00.github.io/LoonManual/#/cn/policygroup?id=select-%e7%ad%96%e7%95%a5%e7%bb%84
+            return this.parse(Supported.Surge)
+        } else if (platform === Supported.Stash) {
+            // ref: https://stash.wiki/en/proxy-protocols/proxy-groups#select
+            const p = {
+                name: this.name,
+                type: 'select',
+                proxies,
+            }
+            return yaml.dump([p])
         } else if (platform === Supported.Surge) {
+            // ref: https://manual.nssurge.com/policy/group.html#manual-selection-group
             const p: string[] = []
             p.push('select')
             p.push(...proxies)
             return this.name + ' = ' + undefinedFreeJoin(p, ', ')
+        } else if (platform === Supported.Surfboard) {
+            // ref: https://getsurfboard.com/docs/profile-format/proxygroup/select
+            return this.parse(Supported.Surge)
         } else {
             throw new UnsupportedProxyError(this, platform)
         }
