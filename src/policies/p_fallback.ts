6c959b0336f12a85c0ecc1af9727cf3a584de87d
MamoruDS <mamoruds.io@gmail.com>
2023-11-04 16:46:04 +0900

    add: policy.fallback parse for loon, stash and surfboard

--- src/policies/p_fallback.ts
+++ src/policies/p_fallback.ts
@@ -1,13 +1,14 @@
 import * as yaml from 'js-yaml'
 
 import { Supported } from '../types'
-import { optionalArgs, undefinedFreeJoin } from '../utils'
+import { optionalArgs as oa, undefinedFreeJoin } from '../utils'
 import { BasePolicy, UnsupportedProxyError, ValidPolicyItem } from './base'
 
 class FallbackPolicyProperties {
     url: string
     interval: number
     timeout?: number
+    hidden?: boolean // +surfboard
 }
 
 class FallbackPolicy extends BasePolicy {
@@ -37,12 +38,40 @@ class FallbackPolicy extends BasePolicy {
                 proxies,
             }
             return yaml.dump([p])
-        } else if (platform === Supported.Surge) {
+        } else if (platform === Supported.Loon) {
+            // ref: https://loon0x00.github.io/LoonManual/#/cn/policygroup?id=fallback
             const p: string[] = []
             p.push('fallback')
             p.push(...proxies)
-            p.push(optionalArgs('interval', this.prop.interval))
-            p.push(optionalArgs('timeout', this.prop.timeout))
+            p.push(oa('url', this.prop.url))
+            p.push(oa('interval', this.prop.interval))
+            p.push(oa('max-timeout', this.prop.timeout))
+            return this.name + ' = ' + undefinedFreeJoin(p, ', ')
+        } else if (platform === Supported.Stash) {
+            // ref: https://stash.wiki/en/proxy-protocols/proxy-groups#fallback
+            const p = {
+                name: this.name,
+                type: 'fallback',
+                proxies,
+            }
+            return yaml.dump([p])
+        } else if (platform === Supported.Surge) {
+            // ref: https://manual.nssurge.com/policy/group.html#fallback-group
+            const p: string[] = []
+            p.push('fallback')
+            p.push(...proxies)
+            p.push(oa('interval', this.prop.interval))
+            p.push(oa('timeout', this.prop.timeout))
+            return this.name + ' = ' + undefinedFreeJoin(p, ', ')
+        } else if (platform === Supported.Surfboard) {
+            // ref: https://getsurfboard.com/docs/profile-format/proxygroup/fallback
+            const p: string[] = []
+            p.push('fallback')
+            p.push(...proxies)
+            p.push(oa('url', this.prop.url))
+            p.push(oa('interval', this.prop.interval))
+            p.push(oa('timeout', this.prop.timeout))
+            p.push(oa('hidden', this.prop.hidden ? 'true' : undefined))
             return this.name + ' = ' + undefinedFreeJoin(p, ', ')
         } else {
             throw new UnsupportedProxyError(this, platform)
