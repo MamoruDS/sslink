56dbc05373f8ce390f09096cae545ddabb2fb9ca
MamoruDS <mamoruds.io@gmail.com>
2023-11-04 16:57:51 +0900

    add: policy.load-balance parse for loon, stash and surfboard

--- src/policies/p_loadbalance.ts
+++ src/policies/p_loadbalance.ts
@@ -1,13 +1,16 @@
 import * as yaml from 'js-yaml'
 
 import { Supported } from '../types'
-import { optionalArgs, undefinedFreeJoin } from '../utils'
+import { optionalArgs as oa, undefinedFreeJoin } from '../utils'
 import { BasePolicy, UnsupportedProxyError, ValidPolicyItem } from './base'
 
 class LoadBalancePolicyProperties {
     url?: string
     interval?: number
-    persistent?: boolean // surge only
+    timeout?: number // +loon
+    persistent?: boolean // +surge; equal to stash's strategy.consistent-hashing
+    algorithm?: 'Random' | 'PCC' | 'Round-Robin' // +loon
+    hidden?: boolean // +surfboard
 }
 
 class LoadBalancePolicy extends BasePolicy {
@@ -37,13 +40,49 @@ class LoadBalancePolicy extends BasePolicy {
                 proxies,
             }
             return yaml.dump([p])
-        } else if (platform === Supported.Surge) {
+        } else if (platform === Supported.Loon) {
+            // ref: https://loon0x00.github.io/LoonManual/#/cn/policygroup?id=load-balance
             const p: string[] = []
             p.push('load-balance')
             p.push(...proxies)
+            p.push(oa('url', this.prop.url))
+            p.push(oa('interval', this.prop.interval))
+            p.push(oa('max-timeout', this.prop.timeout))
             p.push(
-                optionalArgs('persistent', this.prop.interval ? 1 : undefined)
+                oa(
+                    'algorithm',
+                    this.prop.algorithm
+                        ? this.prop.algorithm
+                        : this.prop.persistent
+                        ? 'PCC'
+                        : undefined
+                )
             )
+            return this.name + ' = ' + undefinedFreeJoin(p, ', ')
+        } else if (platform === Supported.Stash) {
+            // ref: https://stash.wiki/en/proxy-protocols/proxy-groups#load-balance
+            const p = {
+                name: this.name,
+                type: 'load-balance',
+                strategy: this.prop.persistent
+                    ? 'consistent-hashing'
+                    : 'round-robin',
+                proxies,
+            }
+            return yaml.dump([p])
+        } else if (platform === Supported.Surge) {
+            // ref: https://manual.nssurge.com/policy/group.html#load-balance-group
+            const p: string[] = []
+            p.push('load-balance')
+            p.push(...proxies)
+            p.push(oa('persistent', this.prop.interval ? 1 : undefined))
+            return this.name + ' = ' + undefinedFreeJoin(p, ', ')
+        } else if (platform === Supported.Surfboard) {
+            // ref: https://getsurfboard.com/docs/profile-format/proxygroup/load-balance
+            const p: string[] = []
+            p.push('load-balance')
+            p.push(...proxies)
+            p.push(oa('hidden', this.prop.hidden ? 'true' : undefined))
             return this.name + ' = ' + undefinedFreeJoin(p, ', ')
         } else {
             throw new UnsupportedProxyError(this, platform)
